1a93d74600234f1bd349f2f2d6bbf072f5233734
muhammed-noufal <muhammed.noufal@citrusinformatics.com>
2018-09-03 17:26:07 +0530

    Implement intro screen

---
+++ src/pages/video/video.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NavController, IonicPage } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-video',
+  templateUrl: 'video.html'
+})
+export class VideoPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
